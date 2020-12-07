522e8e10007a179883b132576ac0d0c39887a02b
Justin Post <jbpost2@ncsu.edu>
2020-12-07 02:44:33 -0500

    Update starter file to remove old activities

--- files/starterFile.R
+++ files/starterFile.R
@@ -1,8 +1,6 @@
 ###########################################################
 ##Code corresponding to activities in course:
 ##Introduction to Data Science Using R
-##_Your_Name_Here_
-##August 13-14, 2018
 ###########################################################
 
 ###########################################################
@@ -47,110 +45,20 @@ paste(adj, nouns)
 
 #Write R code to do the following:
 
-#1. Go to the gitHub datasets folder and look at the breastcancer.dat file. Click on the "raw" button.
+#1. Read in the BreastCancer.dat data file from the URL 'https://www4.stat.ncsu.edu/~online/datasets/BreastCancer.dat'. Save the R object as cancerData.
 
-#2. Read in the BreastCancer.dat data file using the URL you got for the gitHub page after clicking "raw". Note this is a tab delimited file.  Save the R object as cancerData.
+#2. Read in the mosquito.txt file from 'https://www4.stat.ncsu.edu/~online/datasets/mosquito.txt'. Save the R object as mosquitoData.
 
-#3. Repeat the above process to get a link to read in the mosquito.txt file. Save the R object as mosquitoData.
+#3. Read in the mosquito2.txt file from 'https://www4.stat.ncsu.edu/~online/datasets/mosquito2.txt'. Note this is a tab delimited file. Save the R object as mosquitoData2.
 
-#4. Repeat the above process to get a link to read in the mosquito2.txt file. Note this is a tab delimited file. Save the R object as mosquitoData2.
-#4a. Use the code below to create a combined version of the mosquito data sets. Save the object as mosquitoFullData.
+#3a. Use the code below to create a combined version of the mosquito data sets. Save the object as mosquitoFullData.
 rbind(mosquitoData, mosquitoData2)
-#4b. Write the mosquitoFullData out to a .csv file with name of your choice.
 
-#5. Repeat the above process to get a link to read in the effort.dta file. Note that this is a stata file. Right click on view raw and copy the link address. Save the R object as effortData.
+#3b. Write the mosquitoFullData out to a .csv file with name of your choice.
 
-
-###########################################################
-##Manipulating Data
-###########################################################
-
-#Do not overwrite the data set in any of the steps below unless specified. We'll use the cancerData object read in during a previous activity.  Try to use chaining/piping where possible! Write R code to do the following:
-
-#in case you haven't read the data in
-cancerData <- read_tsv(file = "https://raw.githubusercontent.com/jbpost2/DataScienceR/master/datasets/BreastCancer.dat")
-
-#1. Have R sort the data set by youngest to oldest.
-
-#2. Have R print out only women who were over 50.
-
-#3. Have R print out only women who were over 50 and premenopausal.
-
-#4. Have R print out only women who were (over 50 and premenopausal) or (under 50 and Postmenopausal).
-
-#5. Have R find the average 'size' of the tumor for each of the previous three groups. You'll need one statement for each group to complete this.
-
-#6. Have R create a new variable called 'flag' that is "flag" if the women were (over 50 and premenopausal) or (under 50 and Postmenopausal) and "no flag" if they were not. This variable should be added to the data frame.
-
-#7. Have R find the average 'size' of the tumor for the "flag" and "no flag" groups in one R statment.
-
-#8. Have R find the number of women that were premenopausal and Postmenopausal, respectively.  Hint use summarize with n() as your function: ex: summarize(count = n())
-
-#9. Create a new data frame called abbCancerData that only has the id, size, grade, and flag variables.
+#4. Read in the effort.dta file form the URL 'https://www4.stat.ncsu.edu/~online/datasets/effort.dta'. Note that this is a stata file. Save the R object as effortData.
 
 
-###########################################################
-##Categorical Plots
-###########################################################
 
-#Recall the Lahman package that has data about baseball players. Consider the "Master" data set. Write R code to do the following:
-
-#1. Convert the Master data set to a tibble
-
-#2. Create a table that gives the frequency of players by birth months.
-
-#3. Create a table that gives the proportion of players by birth months. (Proportion = # in category/total #)
-                                                                        
-#4. Create a bar chart of the player birth month frequencies. Give the plot appropriate labels.
-                                                                      
-#5. Create a two-way contingency table that gives the frequencies of how the player bats ("bats") and the way the player throws ("throws").
-                                                                      
-#6. Create a stacked bar chart of the player bat and throw preferences. Give the plot appropriate labels.
-
-
-###########################################################
-##Quantitative Plots
-###########################################################
-
-#We will continue the use of the Lahman package. This time we will look at the Batting data set. Write R code to do the following:
-
-#1. Convert the Batting data set to a tibble.
-
-#2. Create a histogram of the games played variable and overlay a smoothed density.
-
-#3. Create side by side box plots of the hits (H) variable with the lgID as the grouping variable.
-
-#4. Find the 'summary' of the hits variable.
-
-#5. Find the total number of homeruns for each year.
-
-#6. Report the correlation between AB and H. Note: The NA values can cause a problem here. Check the options on the cor function.
-
-#7. Create a scatter plot of the AB and H variables (AB on x axis). Label and title appropriately.
-
-#8. Add a regression line to the plot of your choosing.
-
-#9. Color the points by the number of homeruns.
-
-#10. Make all of the points smaller.
-
-
-###########################################################
-##Basic Analyses
-###########################################################
-
-#We'll use the data set built into ggplot2 called msleep. That is, once you've loaded the ggplot2 library the data set will be available to you.  Write R code to do the following:
-
-#1. Fit a SLR model with ln (log in R) of body weight as your response and time awake as your predictor.
-
-#2. Obtain the fitted regression equation.
-
-#3. Predict the log body weight for a mammal that is awake 18 hours
-
-#4. Transform the prediction to the body weight scale
-
-#5. Obtain confidence and prediction intervals for a mammal that is awake 18 hours
-
-#6. Obtain diagnostic plots for the model fit.
 
 
